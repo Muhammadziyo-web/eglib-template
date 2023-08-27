536e2c6946e01a334d9f8519b41d8a0fd8ea09a4
Muhammadziyo-web <mrvaliyev2006@gmail.com>
2023-08-27 22:43:56 +0500

    Server port updated

--- src/server/server.js
+++ src/server/server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-   
+    
 app.get("/",(req, res) => {
         res.status(200).json("Hello World!");
 });
